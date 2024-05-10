602cf2a0dbb333849022c259975f66b1bed7a35c
basuabhirup <basu.abhirup27@gmail.com>
2024-05-10 14:56:07 +0530

    Implement search functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 from . import util
 
 
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+def index(request):    
+    if not request.GET.get("q"):   
+        return render(request, "encyclopedia/index.html", {
+            "heading": "All Pages",
+            "entries": util.list_entries()
+        })
+        
+    query = request.GET["q"]
+    
+    if (util.get_entry(query) is not None):
+        return HttpResponseRedirect(f"wiki/{query}")
+    else:
+        entries = []
+        for entry in util.list_entries():
+            if query in entry:
+                entries.append(entry)
+        
+        return render(request, "encyclopedia/index.html", {
+            "heading": f"Search results for '{query}'",
+            "entries": entries
+        })
 
 def wiki(request, title):
     entry = util.get_entry(title)
